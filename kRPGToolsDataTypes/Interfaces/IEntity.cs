1e674cad056448d6972e2cc02a3fc63cb5366179
rKeeling75 <randykeeling@gmail.com>
2015-06-27 16:36:11 -0400

    Adding in beginning interfaces

--- kRPGToolsDataTypes/Interfaces/IEntity.cs
+++ kRPGToolsDataTypes/Interfaces/IEntity.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace kRPGToolsDataTypes
+namespace kRPGToolsDataTypes.Interfaces
 {
-    public class Class1
+    interface IEntity
     {
     }
 }
